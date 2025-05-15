1a1386b3a9bce1f93dcfdd9b02821d2735ed5885
hanelmouta <127690334+hanelmouta@users.noreply.github.com>
2025-05-15 12:25:05 +0200

    Update decompress.cc

--- fuzz/decompress.cc
+++ fuzz/decompress.cc
@@ -3,71 +3,38 @@
 #include <cstdio>
 #include <csetjmp>
 #include "jpeglib.h"
-
-struct my_error_mgr {
-    struct jpeg_error_mgr pub;
-    jmp_buf setjmp_buffer;
-};
-
-METHODDEF(void) my_error_exit(j_common_ptr cinfo) {
-    my_error_mgr* myerr = (my_error_mgr*) cinfo->err;
-    longjmp(myerr->setjmp_buffer, 1);
-}
+#include "jchuff.h" // Ensure this header provides the declaration for encode_one_block
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-    if (size < 64) { // Skip inputs smaller than a single 8x8 block
-        return 0;
+    if (size < sizeof(JBLOCK)) {
+        return 0; // Skip inputs smaller than a single block
     }
 
     struct jpeg_compress_struct cinfo;
-    struct my_error_mgr jerr;
+    struct jpeg_error_mgr jerr;
 
-    cinfo.err = jpeg_std_error(&jerr.pub);
-    jerr.pub.error_exit = my_error_exit;
-
-    if (setjmp(jerr.setjmp_buffer)) {
-        jpeg_destroy_compress(&cinfo);
-        return 0;
-    }
-
+    cinfo.err = jpeg_std_error(&jerr);
     jpeg_create_compress(&cinfo);
 
-    // Set up a dummy destination manager
-    unsigned char dummy_output[1024];
-    struct jpeg_destination_mgr dest_mgr;
-    dest_mgr.init_destination = [](j_compress_ptr cinfo) {};
-    dest_mgr.empty_output_buffer = [](j_compress_ptr cinfo) -> boolean { return TRUE; };
-    dest_mgr.term_destination = [](j_compress_ptr cinfo) {};
-    dest_mgr.next_output_byte = dummy_output;
-    dest_mgr.free_in_buffer = sizeof(dummy_output);
-    cinfo.dest = &dest_mgr;
-
-    // Set up compression parameters
-    cinfo.image_width = 8;  // Width of one block
-    cinfo.image_height = 8; // Height of one block
-    cinfo.input_components = 1; // Grayscale
+    // Mock compression parameters
+    cinfo.image_width = 8;
+    cinfo.image_height = 8;
+    cinfo.input_components = 1;
     cinfo.in_color_space = JCS_GRAYSCALE;
     jpeg_set_defaults(&cinfo);
 
-    // Start compression
-    jpeg_start_compress(&cinfo, TRUE);
+    // Prepare a single block of data
+    JBLOCK block;
+    memcpy(block, data, sizeof(JBLOCK));
 
-    // Prepare a single 8x8 block of data
-    JSAMPLE row[8];
-    JSAMPROW row_pointer[1];
-    for (int i = 0; i < 8; ++i) {
-        row[i] = data[i % size]; // Fill row with fuzzed data
-    }
-    row_pointer[0] = row;
+    // Mock Huffman tables
+    c_derived_tbl dctbl, actbl;
+    memset(&dctbl, 0, sizeof(dctbl));
+    memset(&actbl, 0, sizeof(actbl));
 
-    // Write the block (indirectly calls `encode_one_block`)
-    for (int i = 0; i < 8; ++i) {
-        jpeg_write_scanlines(&cinfo, row_pointer, 1);
-    }
+    // Call encode_one_block directly
+    encode_one_block(&cinfo, block, 0, &dctbl, &actbl);
 
-    // Finish compression
-    jpeg_finish_compress(&cinfo);
     jpeg_destroy_compress(&cinfo);
-
     return 0;
 }
